6331838d8cac0bbe617917c1b0969d54b33ef2db
smutallib97 <smutallib97@gmail.com>
2022-11-18 22:30:35 +0530

    Added: Ability to create address book table

--- AddressBookService.sql
+++ AddressBookService.sql
@@ -1,1 +1,5 @@
 create database Address_Book_Service;
+show databases;
+use Address_Book_Service;
+create table AddressBook (firstName varchar(20) not null, lastName varchar(20) not null, address varchar(100) not null,
+city varchar(50) not null, state varchar(50) not null, zipcode int not null, phoneNumber double not null, email varchar(100) not null);
